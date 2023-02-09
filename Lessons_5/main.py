1f169fec1a47041cb7da3131c0405e9f5d6011b0
Shevtsov Sergey <shevadotka@gmail.com>
2023-02-09 14:15:26 +0200

    create FSM and save data

--- Lessons_5/main.py
+++ Lessons_5/main.py
@@ -3,7 +3,7 @@ from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.dispatcher import FSMContext
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 from aiogram.dispatcher.filters import Text
-from aiogram.dispatcher.filters.state import State, StatesGroup, StatesGroupMeta
+from aiogram.dispatcher.filters.state import State, StatesGroup
 import logging
 import setings
 import os
